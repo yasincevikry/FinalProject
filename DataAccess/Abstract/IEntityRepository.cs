e45aa639c74b023e3c9c896f72f54d81505a7ad1
yasin <yasin@DESKTOP-JBUPJI1>
2023-02-08 20:12:42 +0300

    Add project files.

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,22 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    //generic contraint
+    //class : refererans tip
+    //IEntity : IEntity olabilir veya IEntity implemente eden bir nesne olabilir
+    //new() : newlenebilir olmalı
+    public interface IEntityRepository<T> where T:class,IEntity,new()
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter=null );
+        T Get(Expression<Func<T, bool>> filter = null); 
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        
+    }
+}
---
+++ Entities/Concrete/Customer.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Customer:IEntity
+    {
+        public string CustomerId { get; set; }
+        public string  ContactNanme { get; set; }
+        public string  CompanyName { get; set; }
+        public string City { get; set; }
+        
+
+    }
+}
