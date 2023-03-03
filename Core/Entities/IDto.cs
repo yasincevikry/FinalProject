7268c12de1ac6628cdd61847929b31032dd4c532
Yasin ÇEVİK <yasincevik34@hotmail.com.tr>
2023-03-03 12:48:28 +0300

    03.03.2023

--- Core/Entities/IDto.cs
+++ Core/Entities/IDto.cs
@@ -1,5 +1,9 @@
-﻿namespace Core.Entites
-{ 
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Entities
+{
     public interface IDto
     {
 
--- Entities/DTOs/ProductDetailDto.cs
+++ Entities/DTOs/ProductDetailDto.cs
@@ -1,4 +1,5 @@
 ﻿using Core.Entites;
+using Core.Entities;
 using System;
 using System.Collections.Generic;
 using System.Text;
